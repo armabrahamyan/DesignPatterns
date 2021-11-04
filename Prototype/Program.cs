b8203e871c7aa18f4152451ba082e345304fbd83
Armen1998 <armena1998@yahoo.com>
2021-11-04 10:54:17 +0400

    prototype initial commit

---
+++ Prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = new Prototype
+            {
+                Class = "Biological system",
+                State = string.Empty
+            };
+
+            var human = prototype.Clone();
+            human.Class = "Human";
+            human.State += "General behavior of human;";
+
+            var man = human.Clone();
+            man.Class = "Man";
+            man.State += "Man Behavior";
+
+            var women = human.Clone();
+            women.Class = "Women";
+            women.State += "Women behavior";
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Prototype
+    {
+        public string Class { get; set; }
+        public string State { get; set; }
+
+        public Prototype Clone()
+        {
+            return MemberwiseClone() as Prototype;
+        }
+    }
+}
